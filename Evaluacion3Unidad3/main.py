adb890d52a769be439a8212ed3254215e2a7baec
crisjrrbdhn <cris191970.je@gmail.com>
2023-11-18 00:33:08 -0300

    Proyecto de Ejercicios U3

---
+++ Evaluacion3Unidad3/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ejercicio1', methods=['GET', 'POST'])
+def ejercicio1():
+    if request.method == 'POST':
+        # notas
+        nota1 = float(request.form['nota1'])
+        nota2 = float(request.form['nota2'])
+        nota3 = float(request.form['nota3'])
+        asistencia = float(request.form['asistencia'])
+
+        # Calcular el promedio de las notas
+        promedio = (nota1 + nota2 + nota3) / 3
+
+        #verificacion
+        if promedio >= 40 and asistencia >= 75:
+            estado = 'Aprobado'
+        else:
+            estado = 'Reprobado'
+
+        return render_template('ejercicio1.html', promedio=promedio, estado=estado)
+
+    return render_template('ejercicio1.html')
+
+@app.route('/ejercicio2', methods=['GET', 'POST'])
+def ejercicio2():
+    if request.method == 'POST':
+        # nombres para formulario
+        nombre1 = request.form['nombre1']
+        nombre2 = request.form['nombre2']
+        nombre3 = request.form['nombre3']
+
+        nombres = [nombre1, nombre2, nombre3]
+        nombre_mas_largo = max(nombres, key=len)
+        longitud_nombre_mas_largo = len(nombre_mas_largo)
+
+        return render_template('ejercicio2.html', nombre_mas_largo=nombre_mas_largo, longitud=longitud_nombre_mas_largo)
+
+    return render_template('ejercicio2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
